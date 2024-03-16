4de9a34d5f97383adb05fcc7ccda435a9c7f3c6e
M Henthoiba <111609721+hitmanbuzz@users.noreply.github.com>
2024-03-16 14:49:29 +0530

    better function call

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,19 @@ use image::get_image;
 
 fn main() {
     let domain = String::from("https://readm.today");
-    let comic_url = String::from("https://readm.today/manga/magic-emperor");
-    result(domain, comic_url);
+    let comic_url = String::from("https://readm.today/manga/the-all-knowing-cultivator");
+    println!("{:#?}",result(domain, comic_url));
 }
 
-/// Use for checking for better response type
-fn result(domain: String, comic_url: String) {  
-    let mut chapter_counter = 0;
+/// Use for checking better response type
+fn result(domain: String, comic_url: String) -> Vec<Result<Vec<String>, Box<(dyn std::error::Error + 'static)>>> {
     let response = get_comic_chapters(comic_url, domain.clone());
-    let chapter_url: Vec<String> = Vec::new();
+    let mut chapter_url = Vec::new();
     if let Ok(u) = response {
         for uri in u {
             let result = get_url(uri, domain.clone());
-            println!("Chapter: {}", chapter_counter);
-            println!("{:#?}", result);
-            println!("\n");
-            chapter_counter += 1;
+            chapter_url.push(result);
         }
     }
+    return chapter_url;
 }
