f62cc0fae3ba69cfb38dd0c849cadf6c166a1709
M Henthoiba <111609721+hitmanbuzz@users.noreply.github.com>
2024-03-22 05:15:44 +0530

    merge function remove as it will be implemented in different method

--- src/source/readm.rs
+++ src/source/readm.rs
@@ -1,8 +1,7 @@
 use std::fs;
-
-use image::ImageBuffer;
 use reqwest::blocking::get;
 
+/// READM.TODAY (readm.today) scrapper
 pub fn readm() {
     let domain = String::from("https://readm.today");
     let comic_url = String::from("https://readm.today/manga/the-all-knowing-cultivator");
@@ -12,8 +11,7 @@ pub fn readm() {
     let comic_dir = comic_title;
     let mut chapter_counter = 0;
     let mut page_counter = 1;
-
-    // println!("{:#?}",result(domain, comic_url));
+    
     for i in readm_result(domain, comic_url) {
         if let Ok(uri) = i {
             let comic_chapter_dir = format!("{}/{}/chapter_{}", parent_dir, comic_dir, chapter_counter);
@@ -138,39 +136,4 @@ fn get_comic_chapters(comic_url: String, domain: String) -> Result<Vec<String>, 
         comic_url_container.reverse();
     }
     return Ok(comic_url_container)
-}
-
-/// Merging plan will not be used for right now as it is not needed
-///
-/// It is not build yet
-#[allow(dead_code)]
-fn merge_image(url: String, domain: String) -> Result<(), Box<(dyn std::error::Error + 'static)>> {
-    let call_url: Vec<String> = get_url(url, domain).expect("Couldn't fetch the url");
-    let mut images = Vec::new();
-    for mut i in call_url {
-        i = i.replace("/", "\\");
-        let image = image::open(i)?;
-        images.push(image);
-    }
-
-    // Get dimensions of the merged image
-    let width: u32 = images.iter().map(|img| img.width()).sum();
-    let height: u32 = images.iter().map(|img| img.height()).max().unwrap_or(0);
-
-    // Create a new image buffer to hold the merged images
-    let mut merged_image = ImageBuffer::new(width, height);
-
-    // Paste images onto the merged image buffer
-    let mut offset_x = 0;
-    for img in images {
-        image::imageops::overlay(&mut merged_image, &img, (offset_x as i32).into(), 0);
-        offset_x += img.width();
-    }
-
-    // Save the merged image
-    merged_image.save("chapter.jpg")?;
-
-    println!("Images merged successfully.");
-
-    Ok(())
 }
