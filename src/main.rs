e99d2b04ac2928758cd9f2bccb3c266454ab1396
M Henthoiba <111609721+hitmanbuzz@users.noreply.github.com>
2024-03-21 19:30:40 +0530

    testing the new concatenate images

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 mod source;
+mod image;
 
+use std::fs;
 use std::io::{self, stdout, Write};
-
 use crate::source::readm::readm;
+use crate::image::join_images;
 
-fn main() {
-    if cli_options() == "1" {
-        readm();
+fn main() -> std::io::Result<()> {
+    let dir_path = String::from("comic/millennial_cultivator/chapter_0/");
+    let mut full_images_path = vec![];
+
+    // Read the contents of the directory
+    let entries = fs::read_dir(dir_path.clone())?;
+
+    // Iterate over the directory entries
+    for entry in entries {
+        // Unwrap the entry
+        let entry = entry?;
+        // Get the file name as a string
+        let file_name = entry.file_name().into_string().unwrap();
+        // Print the file name
+        full_images_path.push(dir_path.clone() + &file_name);
     }
-    else {
-        println!("Wrong option!!!");
-    }
+    join_images(full_images_path, "joined.jpg".to_owned());
+
+    Ok(())
 }
 
 fn cli_options() -> String {
